d72ba54d376032a061d39912b324b5fc00e869f8
Dipanjan441 <dipanjantripathi441@gmail.com>
2025-02-23 19:38:04 +0530

    DB connected

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
-console.log('app');
+import dotenv from "dotenv";
+import connectDB from './db/index.js';
+
+dotenv.config({
+    path: './env'
+})
+
+
+connectDB();
+
+// (async()=>{
+//     try{
+//         await mongoose.connect(`${process.env.MOGODB_URL}/${DB_NAME}`)
+//         app.on('error',(err)=> {
+//             console.error('Application error',err)
+//             throw err
+//         })
+
+//         app.listen(process.env.PORT,()=> {
+//             console.log('app is running on port no',process.env.PORT);
+//         })
+
+//     } catch(err) {
+//         console.error('error is',err)
+//         throw err
+//     }
+// })()
